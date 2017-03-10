7b7eba25809e04c19bb067d327fcf9bf344ea96f
Sun Daowen <sundaowen@gmail.com>
2017-03-10 21:56:05 +0800

    LinuxMint18

---
+++ test-LinuxMint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function pause {
+    read -rsp "Press any key to continue . . . " -n 1 && echo
+}
+
+cwdir=`pwd`
+rootdir=`dirname "$0"`
+cd "$rootdir"
+rootdir=`pwd`
+rm -rf test
+mkdir test
+cd test
+cmake .. 2>"../LinuxMint-Unix Makefiles.txt"
+cd ..
+rm -rf test
+cd "$cwdir"
+pause
