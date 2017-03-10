419cb0764adfa78bdff0c245497d5df44d05d205
dnasdw <sundaowen@gmail.com>
2017-03-10 15:19:23 +0800

    MSYS2+MinGW32+MinGW64

---
+++ test-MSYS2-MinGW64.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function pause {
+    read -rsp "Press any key to continue . . . " -n 1 && echo
+}
+
+cwdir=`pwd`
+rootdir=`dirname "$0"`
+cd "$rootdir"
+rootdir=`pwd`
+rm -rf test
+mkdir test
+cd test
+mv /usr/bin/sh /usr/bin/sh_backup
+cmake -G "MinGW Makefiles" .. 2>"../MSYS2-MinGW64-MinGW Makefiles.txt"
+mv /usr/bin/sh_backup /usr/bin/sh
+cd ..
+rm -rf test
+cd "$cwdir"
+pause
