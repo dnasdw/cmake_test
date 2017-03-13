8e66e8a8e6feb612c7eaa9f491d089455dd7b766
Sun Daowen <sundaowen@gmail.com>
2017-03-13 21:08:34 +0800

    win10 1607+win10 1607+vs2008+vs2010+vs2012+vs2013+vs2015+vs2017 nmake

---
+++ test-VS2012 x86_amd64-NMake Makefiles.bat
@@ -0,0 +1,11 @@
+SET cwdir=%CD%
+SET rootdir=%~dp0
+CD /D "%rootdir%"
+RD /S /Q test
+MD test
+CD test
+cmake -G "NMake Makefiles" .. 2>"..\Visual Studio 11 2012 x86_amd64 NMake Makefiles.txt"
+CD ..
+RD /S /Q test
+CD /D "%cwdir%"
+PAUSE
---
+++ test-VS2017 x86_amd64-NMake Makefiles.bat
@@ -0,0 +1,11 @@
+SET cwdir=%CD%
+SET rootdir=%~dp0
+CD /D "%rootdir%"
+RD /S /Q test
+MD test
+CD test
+cmake -G "NMake Makefiles" .. 2>"..\Visual Studio 15 2017 x86_amd64 NMake Makefiles.txt"
+CD ..
+RD /S /Q test
+CD /D "%cwdir%"
+PAUSE
